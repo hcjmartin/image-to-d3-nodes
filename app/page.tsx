fd290d250309c7ce700d0a32df5c3127b912541a
hcjmartin <hcjmartin.work@gmail.com>
2024-01-22 12:20:30 +1100

    FIX: resolve mobile scaling issue

--- app/page.tsx
+++ app/page.tsx
@@ -6,6 +6,9 @@ export default function Home() {
   const [nightmode, setNightmode] = useState(false);
   const [tooltipVisible, setTooltipVisible] = useState(true);
   const [userDataUrl, setUserDataUrl] = useState<string>();
+  const [screenLoaded, setScreenLoaded] = useState(false);
+
+  console.clear();
 
   var windowSize = useRef([100, 100]);
   var isMobile = false;
@@ -13,21 +16,10 @@ export default function Home() {
   var initialImageUrl = "/images/canvalogosm2.png";
 
   const inputFileRef = useRef<any>();
-
-  if (typeof window !== "undefined") {
-    windowSize.current = [window.innerWidth, window.innerHeight];
-
-    let userAgent = navigator.userAgent.toLowerCase();
-    isMobile = /iphone|ipad|ipod|android|blackberry|windows phone/g.test(
-      userAgent
-    );
-  }
-
   const onInputClick = () => {
     inputFileRef.current.click();
   };
 
-  console.clear();
   console.log(
     "/// Hey! Harry here. This silly little experiment converts a local png to a d3 graph of nodes."
   );
@@ -40,6 +32,28 @@ export default function Home() {
 
   useEffect(() => {
     setTimeout(() => setTooltipVisible(false), 4000);
+    setTimeout(() => {
+      if (typeof window !== "undefined") {
+        let userAgent = navigator.userAgent.toLowerCase();
+
+        isMobile = /iphone|ipad|ipod|android|blackberry|windows phone/g.test(
+          userAgent
+        );
+
+        windowSize.current = [window.innerWidth, window.innerHeight];
+
+        if (isMobile) {
+          if (window.visualViewport) {
+            windowSize.current = [
+              window.visualViewport?.width,
+              window.visualViewport?.height,
+            ];
+          }
+        }
+
+        setScreenLoaded(true);
+      }
+    }, 1000);
   }, []);
 
   return (
@@ -47,111 +61,116 @@ export default function Home() {
       className="flex min-h-screen flex-col items-center justify-between p-24"
       style={{ backgroundColor: nightmode ? "black" : "white" }}
     >
-      <div
-        style={{
-          display: "block",
-          position: "absolute",
-          width: "100%",
-          height: "100%",
-          top: 0,
-          left: 0,
-        }}
-      >
-        <Graph
-          width={windowSize.current[0]}
-          height={windowSize.current[1]}
-          imageToConvertPath={initialImageUrl}
-          splashImagePath="/images/harry256li.png"
-          splashClickLink="https://www.linkedin.com/in/hcjmartin/"
-          fileDataUrl={userDataUrl}
-          isMobile={isMobile}
-        ></Graph>
-      </div>
-      <div
-        style={{
-          display: "flex",
-          position: "absolute",
-          margin: 24,
-          bottom: 0,
-          right: 0,
-          alignItems: "end",
-          flexDirection: "column",
-          textAlign: "right",
-        }}
-      >
-        <p style={{ color: nightmode ? "white" : "black" }}>
-          Move your mouse over the graphic, help me make a splash at Canva 😄{" "}
-        </p>
-      </div>
-      <div
-        style={{
-          position: "absolute",
-          margin: 24,
-          top: 0,
-          right: 0,
-          marginLeft: 6,
-          display: "flex",
-          flexDirection: "row",
-          textAlign: "center",
-        }}
-      >
-        <p
-          style={{ color: nightmode ? "white" : "black", margin: "auto" }}
-          className={!tooltipVisible ? "fade-out" : ""}
-        >
-          Choose your own image{" -> "}
-        </p>
-        <div
-          style={{
-            width: 24,
-            height: 24,
-            borderRadius: 10,
-            margin: 6,
-            borderWidth: 2,
-            borderColor: nightmode ? "white" : "black",
-          }}
-        >
-          <img
-            src={userDataUrl ?? initialImageUrl}
-            alt="logo"
-            style={{ width: "100%" }}
-            onClick={onInputClick}
-          />{" "}
-          <input
-            type="file"
-            name="myImage"
-            ref={inputFileRef}
-            accept='image/png, image/jpeg'
-            onChange={(event) => {
-              let targetFile = event.target.files;
-              if (targetFile !== null) {
-                var uploadedFile = targetFile[0];
-                var reader = new FileReader();
-
-                reader.readAsDataURL(uploadedFile);
-                reader.onloadend = (dataUrl) => {
-                  console.log("image loaded");
-                  let result = reader.result;
-                  if (result !== null) {
-                    setUserDataUrl(result.toString());
-                  }
-                };
-              }
+      {screenLoaded && (
+        <div>
+          <div
+            style={{
+              display: "block",
+              position: "absolute",
+              width: "100%",
+              height: "100%",
+              top: 0,
+              left: 0,
             }}
-            style={{ visibility: "hidden" }}
-          />
+          >
+            <Graph
+              width={windowSize.current[0]}
+              height={windowSize.current[1]}
+              imageToConvertPath={initialImageUrl}
+              splashImagePath="/images/harry256li.png"
+              splashClickLink="https://www.linkedin.com/in/hcjmartin/"
+              fileDataUrl={userDataUrl}
+              isMobile={isMobile}
+            ></Graph>
+          </div>
+          <div
+            style={{
+              display: "flex",
+              position: "absolute",
+              margin: 24,
+              bottom: 0,
+              right: 0,
+              alignItems: "end",
+              flexDirection: "column",
+              textAlign: "right",
+            }}
+          >
+            <p style={{ color: nightmode ? "white" : "black" }}>
+              Move your mouse over the graphic, help me make a splash at Canva
+              😄{" "}
+            </p>
+          </div>
+          <div
+            style={{
+              position: "absolute",
+              margin: 24,
+              top: 0,
+              right: 0,
+              marginLeft: 6,
+              display: "flex",
+              flexDirection: "row",
+              textAlign: "center",
+            }}
+          >
+            <p
+              style={{ color: nightmode ? "white" : "black", margin: "auto" }}
+              className={!tooltipVisible ? "fade-out" : ""}
+            >
+              Choose your own image{" -> "}
+            </p>
+            <div
+              style={{
+                width: 24,
+                height: 24,
+                borderRadius: 10,
+                margin: 6,
+                borderWidth: 2,
+                borderColor: nightmode ? "white" : "black",
+              }}
+            >
+              <img
+                src={userDataUrl ?? initialImageUrl}
+                alt="logo"
+                style={{ width: "100%" }}
+                onClick={onInputClick}
+              />{" "}
+              <input
+                type="file"
+                name="myImage"
+                ref={inputFileRef}
+                accept="image/png, image/jpeg"
+                onChange={(event) => {
+                  let targetFile = event.target.files;
+                  if (targetFile !== null) {
+                    var uploadedFile = targetFile[0];
+                    var reader = new FileReader();
+
+                    reader.readAsDataURL(uploadedFile);
+                    reader.onloadend = (dataUrl) => {
+                      console.log("image loaded");
+                      let result = reader.result;
+                      if (result !== null) {
+                        setUserDataUrl(result.toString());
+                      }
+                    };
+                  }
+                }}
+                style={{ visibility: "hidden" }}
+              />
+            </div>
+            <div
+              style={{
+                width: 24,
+                height: 24,
+                margin: 6,
+                borderRadius: 10,
+                backgroundColor: nightmode ? "white" : "black",
+              }}
+              onClick={() => setNightmode(!nightmode)}
+            />
+          </div>
         </div>
-        <div
-          style={{
-            width: 24,
-            height: 24,
-            margin: 6,
-            borderRadius: 10,
-            backgroundColor: nightmode ? "white" : "black",
-          }}
-          onClick={() => setNightmode(!nightmode)}
-        />
-      </div>
+      )}
     </main>
   );
 }
